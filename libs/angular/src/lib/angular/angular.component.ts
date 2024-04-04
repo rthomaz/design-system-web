bee1f642e165a208a8c088e09afa356b988f5394
Rodrigo Thomaz <rodrigo@rthomaz.com.br>
2024-04-04 02:32:19 -0300

    Criando pipeline de teste

---
+++ libs/angular/src/lib/angular/angular.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ds-angular',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>angular works!</p>`,
+  styles: ``,
+})
+export class AngularComponent {}
---
+++ libs/icons/src/lib/icons/icons.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ds-icons',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>icons works!</p>`,
+  styles: ``,
+})
+export class IconsComponent {}
