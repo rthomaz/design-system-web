1e9c294c6652bb9f2d320f7fe24d4c10626aa56e
Rodrigo Thomaz <rodrigo@rthomaz.com.br>
2024-04-04 02:27:57 -0300

    Criando pipeline de teste

---
+++ libs/assets/src/lib/assets/assets.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ds-assets',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>assets works!</p>`,
+  styles: ``,
+})
+export class AssetsComponent {}
